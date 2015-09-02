a485b6ac53b1ec18d27a80fd61881162954ade63
farthen <monkey@farthen.de>
2015-09-02 03:27:56 +0200

    Make this a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='people',
+      version='0.1',
+      description='People.json database interface',
+      url='http://github.com/wurstmineberg/people',
+      author='Wurstmineberg',
+      author_email='people@wurstmineberg.de',
+      license='MIT',
+      packages=['people'],
+      zip_safe=True)
+
