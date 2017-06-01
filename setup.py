1f6b7890670e1c854449d8c53ffdef878bb8c0bc
Fenhl <fenhl@fenhl.net>
2017-06-01 19:29:40 +0000

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(name='people',
-      version='0.1',
-      description='People.json database interface',
-      url='http://github.com/wurstmineberg/people',
-      author='Wurstmineberg',
-      author_email='people@wurstmineberg.de',
-      license='MIT',
-      packages=['people'],
-      zip_safe=True)
+import setuptools
 
+setuptools.setup(
+    name='people',
+    version='0.1',
+    description='People.json database interface',
+    url='http://github.com/wurstmineberg/people',
+    author='Wurstmineberg',
+    author_email='mail@wurstmineberg.de',
+    license='MIT',
+    packages=['people'],
+    zip_safe=True,
+    install_requires=[
+        'docopt',
+        'dpath',
+        'getpass',
+        'iso8601',
+        'jsonschema',
+        'passlib',
+        'psycopg2',
+        'slacker'
+    ]
+)
